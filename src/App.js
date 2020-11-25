edad776b20309d91ba4dc365236789f21a6d7bf7
Julio Bermudez <julenbero@gmail.com>
2020-11-25 02:09:35 -0500

    Exercise Complete

--- src/App.js
+++ src/App.js
@@ -3,24 +3,49 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="list">
-          <h3>Por hacer:</h3>
-          <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
-          </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
-        </div>
-      </div>
-    )
-  }
-}
+	state = {
+		inputValue : '',
+		todo       : null
+	};
 
+	handleChange = (e) => {
+		this.setState({
+			inputValue : e.target.value
+		});
+	};
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.setState({
+			todo       : this.state.inputValue,
+			inputValue : ''
+		});
+	};
+
+	render() {
+		return (
+			<div className="wrapper">
+				<div className="list">
+					<h3>Por hacer:</h3>
+					<ul className="todo">
+						<li>Sacar la ropa</li>
+						<li>Hacer la cama</li>
+						<li>Leer un rato</li>
+						{this.state.todo && <li>{this.state.todo}</li>}
+					</ul>
+					<form onSubmit={(e) => this.handleSubmit(e)}>
+						<input
+							type="text"
+							id="new-task"
+							placeholder="Ingresa una tarea y oprime Enter"
+							value={this.state.inputValue}
+							onChange={(e) => this.handleChange(e)}
+						/>
+					</form>
+				</div>
+			</div>
+		);
+	}
+}
 
 export default App;
